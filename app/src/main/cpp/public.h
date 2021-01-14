c27e4222c2ee91a61f3d0b0645ea994cc01e375a
hxbreak <hxbreak@qq.com>
2021-01-14 18:58:35 +0800

    init

---
+++ app/src/main/cpp/public.h
@@ -0,0 +1,11 @@
+//
+// Created by hxbreak on 21-1-14.
+//
+
+#ifndef HOOK_DEMO_PUBLIC_H
+#define HOOK_DEMO_PUBLIC_H
+
+extern const unsigned char cacert[];
+extern const unsigned int cacert_len;
+
+#endif //HOOK_DEMO_PUBLIC_H
